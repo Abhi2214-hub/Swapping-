9bc40d2bc49c3a1fdfc42ef1b0061be591259cd4
Abhi2214-hub <68007150+Abhi2214-hub@users.noreply.github.com>
2020-07-08 16:15:21 +0530

    Created Swapping.c
    This file print swapping of two number on screen

---
+++ Swapping.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int a=10 , b=5
+ 
+// Code to swap 'a' and 'b'
+a= a + b; // a now becomes 15
+b= a - b; // b becomes 10
+a= a - b; // a becomes 5
+
+printf(" After Swapping: a =%d, b=%d ,a ,b);
+return 0;
+}
